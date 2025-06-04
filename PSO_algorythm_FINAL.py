0a32cf8426ad43ce6e90a25202b2f929a5b1a9b6
danibe2 <daniel.bermejosch@gmail.com>
2025-06-04 17:16:42 +0200

    Update PSO_algorythm_FINAL.py

--- PSO_algorythm_FINAL.py
+++ PSO_algorythm_FINAL.py
@@ -31,7 +31,7 @@ scaler = StandardScaler()
 X[numeric_cols] = scaler.fit_transform(X[numeric_cols])
 
 # 7. División en entrenamiento y test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 # 3. Definición del PSO (Modificado para incluir alpha)
